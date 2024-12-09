33161b5f50b465ff140052292b08c8b6cc556683
Jay <148691182+ja4y@users.noreply.github.com>
2024-12-09 17:43:53 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// JavaScript koodilla kursorin liikkuminen
+document.body.addEventListener("mousemove", (e) => {
+  const cursor = document.querySelector(".custom-cursor");
+  cursor.style.left = `${e.pageX}px`;
+  cursor.style.top = `${e.pageY}px`;
+});
